bee7755bac8683531be09359eb8da9c1d27f9b0d
MikulasBar <113968592+MikulasBar@users.noreply.github.com>
2023-11-04 19:53:29 +0100

    Add files via upload
    first commit - Plan

---
+++ Utilities.hpp
@@ -0,0 +1,9 @@
+namespace Math
+{
+    //ratio between circumference and radius
+    #define PI 3.141592653589
+    //Euler's constant
+    //base of natural log
+    #define E  2.718281828459
+
+}
---
+++ new.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include "Utilities.hpp"
+
+using namespace Math;
+using namespace std;
+int main() 
+{
+    
+
+
+    float pi = PI;
+    
+    cout << PI;
+    return 0;
+}
+
+
+
+
+
