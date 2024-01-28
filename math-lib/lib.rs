f43e0d68f1ff355c55da7054ff1caf56b56ae8ef
MikulasBar <113968592+MikulasBar@users.noreply.github.com>
2024-01-28 20:27:50 +0100

    Add files via upload

--- math-lib/lib.rs
+++ math-lib/lib.rs
@@ -1,294 +1,211 @@
 
-/// module for math <br>
-/// uses f32 as one and only numeric type
+/// Module for math <br>
+/// Uses f32 as one and only numeric type
 pub mod my_math {
-    use std::ops::{
-        Add, Sub, Mul, Div,
-        AddAssign,
-    };
+    use std::collections::HashMap;
 
     pub const PI: f32 = 3.1415927;
     pub const E: f32 = 2.7182818;
 
-    // pub struct DebugInfo {
-    //     /// Warns you when undefined value is computed
-    //     pub undefined_value_warning: bool,
-
-    //     /// Print all Values that was computed <br>
-    //     /// That includes nested function structs (`FuncObject`)
-    //     pub print_computed_values: bool,
-        
-    // }
-
-    // /// Allows you to print hidden calculations to console <br>
-    // /// All fields are `false` by default
-    // pub static mut DEBUG_INFO: DebugInfo = DebugInfo {
-    //     undefined_value_warning: false,
-    //     print_computed_values: false,
-    // };
-
-
-
-    /// Holds `f32` or it's `Undefined`
-    /// 
-    /// Not designed as alternative to `f32` <br>
-    /// Doesn't have all functions that `f32` has
-    /// 
-    /// Designed for `my_math` module 
-    #[derive(Clone, Debug, PartialEq)]
-    pub enum Value {
-        Num(f32),
-        Undefined,
-    }
-
-    pub const UNDEFINED: Value = Value::Undefined; 
-    
-
-    impl Value {
-
-        /// Unwraps `Value` into `f32` <br>
-        /// Panics if the value is `Undefined`
-        pub fn unwrap(&self) -> f32 {
-            if let Value::Num(n) = self {
-                return n.clone();
-            }
-            panic!("The unwrap method for 'Value' type wasn't succesfull");
-        }
-    }
-
-    impl Add for Value {
-        type Output = Self;
-
-        fn add(self, other: Self) -> Self::Output {
-            match (self, other) {
-                (Value::Num(n), Value::Num(m)) => {
-                    Value::Num(n + m)
-                }
-                _ => Value::Undefined
-            }
-        }
-    }
-
-    impl AddAssign for Value {
-        fn add_assign(&mut self, rhs: Self) {
-            *self = self.clone() + rhs;
-        }
-    }
-
-    impl Sub for Value {
-        type Output = Self;
-
-        fn sub(self, other: Self) -> Self::Output {
-            match (self, other) {
-                (Value::Num(n), Value::Num(m)) => {
-                    Value::Num(n - m)
-                }
-                _ => Value::Undefined
-            }
-        }
-    }
-
-    impl Mul for Value {
-        type Output = Self;
-
-        fn mul(self, other: Self) -> Self::Output {
-            match (self, other) {
-                (Value::Num(n), Value::Num(m)) => {
-                    Value::Num(n * m)
-                }
-                _ => Value::Undefined
-            }
-        }
-    }
-
-    impl Div for Value {
-        type Output = Self;
-
-        fn div(self, other: Self) -> Self::Output {
-            match (self, other) {
-                (Value::Num(n), Value::Num(m)) if m != 0.0 => {
-                    Value::Num(n / m)
-                }
-                _ => Value::Undefined
-            }
-        }
-    }
-
-    impl Into<Value> for f32 {
-        fn into(self) -> Value {
-            Value::Num(self)
-        }    
-    }
-
-
-
-
+    pub type FuncObject = Box<dyn Func>;
+    pub type FuncArgs = HashMap<String, f32>;
 
     pub trait Func {
-        fn get_variables(&self) -> Vec<String>;
-        fn apply(&self, args: &Vec<Value>) -> Value;
+        fn apply(&self, args: &FuncArgs) -> f32;
     }
 
-    mod utilities {
-        use super::*;
 
-        /// Use to warn about invalid arguments <br>
-        /// This doesn't throw error
-        /// 
-        /// Returns `Value::Undefined`
-        pub fn invalid_arguments(args_len: usize, var_len: usize) -> Value {
-            println!("Invalid number of arguments provided: {args_len}, expected: {var_len}");
-            return Value::Undefined;
-        }
+    pub struct PowFunc {
+        power: f32,
+        variable: String,
+        child: Option<FuncObject>
     }
 
-    pub mod func_structs {
-        use super::{
-            *,
-            utilities::*,
-        };
+    pub struct AddFunc {           
+        children: Vec<FuncObject>
+    }
+
+    pub struct MulFunc {
+        children: Vec<FuncObject>
+    }
 
 
-        type FuncObject = Box<dyn Func>;
-        
 
 
-        pub struct PowFunc {
-            pub power: f32,
-
-            variables: Vec<String>,
-            child: Option<FuncObject>
-        }
-
-        pub struct AddFunc { 
-            variables: Vec<String>,          
-            children: Vec<FuncObject>
-        }
-
-    
-
-
-        impl PowFunc {
-            /// Initialise new `PowFunc` with no child
-            pub fn new(power: f32, variable: impl Into<String>) -> Self {
-                PowFunc {
-                    power,
-                    variables: vec![variable.into()],
-                    child: None,
-                }
-            }
-
-            pub fn set_child(&mut self, child: FuncObject) {
-                self.variables = child.get_variables();
-                self.child = Some(child);
-            }
-
-            pub fn remove_child(&mut self, variable: String) {
-                self.child = None;
-                self.variables = vec![variable];
+    impl PowFunc {
+        /// Initialise new `PowFunc` with no child
+        pub fn new(power: f32, variable: impl Into<String>) -> Self {
+            PowFunc {
+                power,
+                variable: variable.into(),
+                child: None,
             }
         }
-    
-        impl Func for PowFunc {
-            fn get_variables(&self) -> Vec<String> {
-                self.variables.clone()
+
+        pub fn set_child(&mut self, child: FuncObject) {
+            self.child = Some(child);
+        }
+
+        pub fn remove_child(&mut self, variable: String) {
+            self.child = None;
+        }
+
+        pub fn has_child(&self) -> bool {
+            if let Option::Some(_) = self.child {
+                return true;
+            }
+            false
+        } 
+    }
+
+    impl Func for PowFunc {
+        fn apply(&self, args: &FuncArgs) -> f32 {
+
+            // has child
+            if let Some(child) = &self.child {
+                let child_result = child.apply(args);
+                return child_result.powf(self.power);
             }
             
-            fn apply(&self, args: &Vec<Value>) -> Value {
-                let var_len = self.variables.len();
-                let args_len = args.len();
-
-                if args_len != var_len {
-                    return invalid_arguments(args_len, var_len);
-                }
-
-
-    
-                // has child
-                if let Some(child) = &self.child {
-                    let child_result = child.apply(args);
-
-                    if let Value::Num(base) = child_result {
-                        return Value::Num(base.powf(self.power));
-                    }
-                }
-                
-                // has not child
-                if let Value::Num(base) = &args[0] {
-                    return Value::Num(base.powf(self.power));
-                }
-
-
-
-                Value::Undefined
-            }
+            // has not child
+            let base = args.get(&self.variable).unwrap();
+            return base.powf(self.power);
         }
-
-        
-
-        impl AddFunc {
-            pub fn new(children: Vec<FuncObject>) -> Self {
-                AddFunc {
-                    variables: vec![],
-                    children
-                }
-            }
-
-            // pub fn add_child(&mut self, child: FuncObject) {
-            //     self.children.push(child);
-            // }
-        }
-
-        impl Func for AddFunc {
-            fn get_variables(&self) -> Vec<String> {
-                self.variables.clone()
-            }
-
-            fn apply(&self, args: &Vec<Value>) -> Value {
-                let mut result = Value::Num(0.0);
-
-                for child in &self.children {
-                    let child_res = child.apply(args);
-                    result += child_res;
-                }
-
-                result
-            }
-
-        }
-
-
     }
+
+
+    impl AddFunc {
+        pub fn new(children: Vec<FuncObject>) -> Self {
+            AddFunc {
+                children
+            }
+        }
+
+        pub fn add_child(&mut self, child: FuncObject) {
+            self.children.push(child);
+        }
+    }
+
+    impl Func for AddFunc {
+        fn apply(&self, args: &FuncArgs) -> f32 {
+            let mut result: f32 = 0.0;
+
+            for child in &self.children {
+                let child_res = child.apply(args);
+                result += child_res;
+            }
+
+            result
+        }
+    }
+    
+
+    impl MulFunc {
+        pub fn new(children: Vec<FuncObject>) -> Self {
+            MulFunc {
+                children
+            }
+        }
+
+        pub fn add_child(&mut self, child: FuncObject) {
+            self.children.push(child);
+        }
+    }
+
+    impl Func for MulFunc {
+        fn apply(&self, args: &FuncArgs) -> f32 {
+            let mut result: f32 = 1.0;
+
+            for child in &self.children {
+                let child_res = child.apply(args);
+                result *= child_res;
+            }
+
+            result
+        }
+    }
+
+
+
+
+
 }
 
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
     use super::my_math::{
         *,
-        func_structs::*,
     };
 
     
 
     #[test]
     fn test_powfunc() {
-        let mut pow1 = PowFunc::new(2.0, "x");
-        let pow2 = PowFunc::new(3.0, "y");
+        let var1 = String::from("x");
+        let var2 = String::from("y");
 
-        const TEST_VAL1: Value = Value::Num(4.0);
-        const TEST_VAL2: Value = Value::Num(2.0);
+        let mut pow_func1 =  PowFunc::new(2.0, var1.clone());
+        let pow_func2 =      PowFunc::new(3.0, var2.clone());
 
-        assert_eq!(pow1.apply(&vec![TEST_VAL1]), Value::Num(16.0));
-        assert_eq!(pow2.apply(&vec![TEST_VAL2]), Value::Num(8.0));
-        assert_eq!(pow1.apply(&vec![UNDEFINED]), UNDEFINED);
+        let mut args1: FuncArgs = HashMap::new();
+        args1.insert(var1,          4.0);
+        args1.insert(var2.clone(),  5.0);
+
+        assert_eq!(pow_func1.apply(&args1),  16.0);
+        assert_eq!(pow_func2.apply(&args1),  125.0);
 
 
-        pow1.set_child(Box::new(pow2));
+        pow_func1.set_child(Box::new(pow_func2));
 
-        assert_eq!(pow1.apply(&vec![TEST_VAL2]), Value::Num(64.0));
-        assert_eq!(pow1.apply(&vec![UNDEFINED]), UNDEFINED);
+        let mut args2: FuncArgs = HashMap::new();
+        args2.insert(var2, 2.0);
 
+        assert_eq!(pow_func1.apply(&args2), 64.0);
     }
 
+    #[test]
+    fn test_addfunc() {
+        let (w, x, y, z) = ("w", "x", "y", "z");
+        
+        let f1 = Box::new(PowFunc::new(1.0, w));
+        let f2 = Box::new(PowFunc::new(2.0, x));
+        let f3 = Box::new(PowFunc::new(3.0, y));
+        let f4 = Box::new(PowFunc::new(4.0, z));
+
+        let add_func = AddFunc::new(vec![f1, f2, f3, f4]);
+
+        let mut args: FuncArgs = HashMap::new();
+
+        args.insert(w.to_string(), 1.0);
+        args.insert(x.to_string(), 2.0);
+        args.insert(y.to_string(), 3.0);
+        args.insert(z.to_string(), 4.0);
+
+
+        assert_eq!(add_func.apply(&args), 288.0);
+    }
+
+    #[test]
+    fn test_mulfunc() {
+        let (w, x, y, z) = ("w", "x", "y", "z");
+        
+        let f1 = Box::new(PowFunc::new(1.0, w));
+        let f2 = Box::new(PowFunc::new(2.0, x));
+        let f3 = Box::new(PowFunc::new(3.0, y));
+        let f4 = Box::new(PowFunc::new(4.0, z));
+
+        let add_func = MulFunc::new(vec![f1, f2, f3, f4]);
+
+        let mut args: FuncArgs = HashMap::new();
+
+        args.insert(w.to_string(), 1.0);
+        args.insert(x.to_string(), 2.0);
+        args.insert(y.to_string(), 3.0);
+        args.insert(z.to_string(), 4.0);
+
+
+        assert_eq!(add_func.apply(&args), 27648.0);
+    }
 }
