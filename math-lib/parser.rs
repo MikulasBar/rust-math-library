1155434fd765db357e25c06f7fd352c6505ee928
MikulasBar <113968592+MikulasBar@users.noreply.github.com>
2024-01-29 21:34:44 +0100

    Regex start

--- math-lib/parser.rs
+++ math-lib/parser.rs
@@ -1,7 +1,16 @@
 /// Used to parse `String` to structure like function <br>
 /// Use the `functions` module from this library
 pub mod parser {
-    
+    use regex::*;
+    use once_cell::sync::Lazy;
+
+    static FUNC_REGEX: Lazy<Regex> = Lazy::new(||
+        Regex::new(r"(\w+)\((.*)\)").unwrap()
+    );
+
+    fn testing_function(str: impl Into<String>) {
+        let mut string = str.into();
+    }
 }
 
 
